b52a4002646525ae83f5c712ef4bfa084c3011cc
univstudentkoyo <21s2024u@student.gs.chiba-u.jp>
2024-08-25 04:24:48 +0900

    add_laravel_base

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\PostController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts',[PostController::class,'index']);
